aa607a7ba8f176f9a21dc3087efab802e5bf63b1
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-11-28 22:13:19 +0800

    add data sync on initialization

--- init-scripts/node2.sql
+++ init-scripts/node2.sql
@@ -1,9 +1,1 @@
-LOAD DATA INFILE '/var/lib/mysql-files/node2_movies.csv'
-INTO TABLE titles
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(tconst, title_type, primary_title, start_year, runtime_minutes, genres);
-
-SELECT 'Node 2 (Movies) - Loaded movie data' AS status, COUNT(*) AS total_rows FROM titles;
+USE imdb_distributed;
